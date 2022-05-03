f90d588c96ff293da701dd8a1408cd09965ef618
Haro1610 <haaronro99@live.com-mx>
2022-05-03 18:57:18 -0500

    Arreglado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
 import { UsersComponent } from './pages/users/users.component';
+import { LoginComponent } from './pages/login/login.component';
 import { DriversComponent } from './pages/drivers/drivers.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RacesComponent } from './pages/races/races.component';
