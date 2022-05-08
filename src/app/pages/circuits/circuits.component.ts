43bc2b3a35c7599b93fbd4c52cf66c573082043f
Haro1610 <haaronro99@live.com-mx>
2022-05-08 17:25:07 -0500

    Se agrega bootstrap y estilos

--- src/app/pages/circuits/circuits.component.ts
+++ src/app/pages/circuits/circuits.component.ts
@@ -6,12 +6,13 @@ import { Circuits } from 'src/app/shared/services/interfaces/circuits-interface'
 @Component({
   selector: 'app-circuits',
   templateUrl: './circuits.component.html',
-  styleUrls: ['./circuits.component.scss']
+  styleUrls: ['./circuits.component.scss'],
 })
 export class CircuitsComponent implements OnInit {
 
   displayedColumns: string[] = [ 'name','address','circuit_distance','phone_number'];
   dataSource: Circuits[] = [];
+  
 
   //public circuits: string = '';
 
@@ -25,3 +26,9 @@ export class CircuitsComponent implements OnInit {
   }
 
 }
+// import { Component } from '@angular/core';
+
+// @Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel-basic.html'})
+// export class NgbdCarouselBasic {
+//   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+// }
