05020e3b4ea6758ac558d3d0f367974f5950c1d8
Haro1610 <haaronro99@live.com-mx>
2022-05-11 02:58:41 -0500

    Se agreagan botones a race results

--- src/app/pages/race-results/race-results.component.ts
+++ src/app/pages/race-results/race-results.component.ts
@@ -5,9 +5,9 @@ import { Router } from '@angular/router';
 import { RaceResultsService } from 'src/app/shared/services/race-results.service';
 import { RaceResults } from 'src/app/shared/services/interfaces/race-results';
 import { AuthServiceService } from 'src/app/shared/services/auth-service.service';
-const ELEMENT_DATA: RaceResults[] = [
-  {_id: '1', results : '1 CQ, 2 VQ 3 AR', number_of_laps: 3,  race_id: "2",race_name: 'Carrerota',date: '1:40'},
-];
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-race-results',
@@ -19,26 +19,50 @@ const ELEMENT_DATA: RaceResults[] = [
 export class RaceResultsComponent implements OnInit {
 
   
-  displayedColumns: string[] = [ 'race_name', 'results', 'number_of_laps','date'];
+  displayedColumns: string[] = [ 'race_name', 'results', 'number_of_laps','date','editar','eliminar'];
   dataSource: RaceResults[] = [];
   public current_race : string = '';
+  form: FormGroup;
 
-  constructor(private Router : ActivatedRoute, private raceResultService : RaceResultsService , private auth: AuthServiceService, private router: Router) { 
+  constructor(private Router : ActivatedRoute, private raceResultService : RaceResultsService , 
+    private auth: AuthServiceService, private router: Router,
+    private formBuilder: FormBuilder,config: NgbModalConfig, 
+    private modalService: NgbModal) { 
     if (!auth.get()){
       this.router.navigate(['/home']);
     }
+    this.form = this.formBuilder.group({
+      results: [],
+      number_of_laps: [],
+      race_id: [],
+      race_name: [],
+      date: [],
+    }
+    );
   }
 
   ngOnInit(): void {
-
       this.Router.params.subscribe( params => {
         this.current_race = params['_id'];
           this.raceResultService.getResults(this.current_race).subscribe( a =>{
             this.dataSource = [a];
         });
-
-        
       });
+      
+  }
+
+  open(content: any) {
+    this.modalService.open(content);
+  }
+
+  sendData(its_new:boolean){
+    if(this.form.valid){
+      const {password, confirm_password} = this.form.getRawValue()
+      console.log('Enviar datos',password,confirm_password);
+      this.router.navigate(['/users']);
+    } else{
+      console.log('Error, faltan datos',this.form);
+    }
   }
 
 }
