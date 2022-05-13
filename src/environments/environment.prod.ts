09d13b8fc3fdb2f36c9c681418146dfb0a7d2575
Haro1610 <79230106+Haro1610@users.noreply.github.com>
2022-05-13 03:25:12 -0500

    Se agregan keys a prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  oogleId: '402060880190-hce6bc8gl31jgi9f8vava68h2ep0suf8.apps.googleusercontent.com',
+  firebaseConfig : {
+    apiKey: "AIzaSyCeXasLZzQYJYmqstVv7UtwJXRZ6WCxIRA",
+    authDomain: "karts-racing.firebaseapp.com",
+    projectId: "karts-racing",
+    storageBucket: "karts-racing.appspot.com",
+    messagingSenderId: "178210329396",
+    appId: "1:178210329396:web:c84b2f7b5410c16f838b61"
+  }
 };
